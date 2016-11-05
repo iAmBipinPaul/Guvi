c3120ce4f2af235907653de12916fad34e2a54d1
Bipin Paul <iambipinpaul@outlook.com>
2016-11-05 16:43:40 +0530

    Program for Converting Strin to Inteher

---
+++ StringToInt.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class StringToInt
+{
+    public static void main(String [] args)
+    {
+Scanner sc=new Scanner(System.in);
+        System.out.println("Ente the Numbr in Stirng Format");
+        String n=sc.nextLine();
+        try
+        {
+ int sum=Integer.parseInt(n);
+ System.out.println("The Convertd Numbris :"+ sum);
+        }
+       catch(Exception e)
+       {
+System.out.println("NOt a valid Integer of String ");
+       }
+
+
+    }
+}
