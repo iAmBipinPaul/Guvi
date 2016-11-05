de391caa73f9fc2266a2ecf365e22ee362710af1
Bipin Paul <iambipinpaul@outlook.com>
2016-11-05 16:06:05 +0530

    Added program for the Factorial

---
+++ Fact.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Fact
+{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ente the number ");
+        int n=sc.nextInt();
+        int sum=1;
+        if(n==0)
+        {
+            sum=1;
+        }
+        else
+          {
+             for(int i=1;i<=n;i++)
+              {
+                 sum=sum*i;
+             }
+            
+           }
+            System.out.println(sum);
+        
+    }
+}
--- StringReverse.java
+++ StringReverse.java
@@ -1,22 +1,39 @@
 import java.util.*;
+
 public class StringReverse{
 
     public static void main(String[] args)
+
     {
+
        /* using builtin StringBuilder  Function 
+
 	 StringBuilder r=new StringBuilder("Bipin");
+
         r.reverse();
+
         System.out.println(r);
+
 */
+
 Scanner sc=new Scanner(System.in);
+
 System.out.println("Enter the String ");
+
  String b=sc.nextLine();
+
         String c="";
+
         for(int i=b.length()-1;i>=0;i--)
+
         {
 
             c=c+b.charAt(i);
+
         }
+
         System.out.println(c);
+
     }
+
 }
